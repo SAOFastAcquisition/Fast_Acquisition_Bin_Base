3c4002b457d19bf70c08f3209a622d924a426abc
Khristine <anatoly.ripak@gmail.com>
2022-01-23 22:49:44 +0300

    Работа со скриптом scan_noise_spectrum.py, запись рисунка в папку.

--- Polyphase/scan_noise_spectrum.py
+++ Polyphase/scan_noise_spectrum.py
@@ -1,7 +1,10 @@
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.fftpack import fft
 from pathlib import Path
+import matplotlib.font_manager as font_manager
+from matplotlib.ticker import MaxNLocator, ScalarFormatter, FixedLocator
 from Supporting_func import path_to_data
 
 
@@ -20,15 +23,49 @@ def low_freq_noise_spectrum(_spectrum, _m):
     return _spectrum_signal_av
 
 
-def plot_low_freq_spec(_spectrum, _delta_t):
+def plot_low_freq_spec(_spectrum, _delta_t, _path_to_picture):
     m, n = _spectrum.shape
     f_max = 1 / _delta_t / 2
     f_min = f_max / n
     arg = np.linspace(f_min, f_max, n)
-    axes = plt.subplots()
+    fig = plt.figure(figsize=(12, 8))
+    plt.grid(b=True, which='major', color='#666666', linestyle='-')
+
+    # Show the minor grid lines with very faint and almost transparent grey lines
+    plt.minorticks_on()
+    plt.grid(b=True, which='minor', color='#999999', linestyle='-', alpha=0.5)
+
+    axes = fig.add_subplot()
+    axes.set_title('Low Noise Spectral Density', fontsize=18)
+    axes.set_yscale('log')
+    axes.set_xscale('log')
+    axes.set_xlabel('Freq, Hz', fontsize=18)
+    axes.set_ylabel('Spectral Density, arb. units', fontsize=20)
     for i in range(m):
-        plt.plot(arg, _spectrum[i, :])
+        axes.plot(arg[0:n//2], _spectrum[i, 0:n//2])
     plt.show()
+
+    fig.savefig(_path_to_picture)
+
+
+def path_to_pic(_file_path, format='png'):
+
+    add_pass0 = 'LF_spectrum_00'
+    _l = len(add_pass0)
+    add_pass1 = add_pass0 + '.' + format
+    if not os.path.isfile(Path(_file_path, add_pass1)):
+        pass
+    else:
+        while os.path.isfile(Path(_file_path, add_pass1)):
+            num = int(add_pass0[_l - 2:_l]) + 1
+            num_str = str(num)
+            if num >= 10:
+                add_pass0 = add_pass0[:_l - 2] + num_str
+            else:
+                add_pass0 = add_pass0[:_l - 2] + '0' + num_str
+            add_pass1 = add_pass0 + '.' + format
+    _path_to_picture = Path(_file_path, add_pass1)
+    return _path_to_picture
 
 
 if __name__ == '__main__':
@@ -36,7 +73,7 @@ if __name__ == '__main__':
     # ********************************** Путь к файлу данных ****************************
     current_catalog = r'2022\Results'  # Текущий каталог (за определенный период, здесь - год)
     current_data_dir = '2022_01_20test'  # Папка с текущими даннымиH:\Fast_Acquisition\2021\Results\2021_09_22test\2021-09-22_01_14bit_pm20
-    current_data_file = '2022-01-20_01test'  # Имя файла с исходными текущими данными без расширенияH:\Fast_Acquisition\2022\Results\2022_01_20test
+    current_data_file = '2022-01-20_03test'  # Имя файла с исходными текущими данными без расширенияH:\Fast_Acquisition\2022\Results\2022_01_20test
     file_path_data, head_path = path_to_data(current_catalog, current_data_dir)
 
     #           ************** Загрузка матрицы спектров *************
@@ -47,8 +84,10 @@ if __name__ == '__main__':
     spectrum2 = spectrum[2]
     spectrum3 = spectrum[3]
     delta_t = 8.1925e-3
+    spectrum_current = np.transpose(spectrum1[:, 20:22])
+    spectrum_signal_av = low_freq_noise_spectrum(spectrum_current, 1024)
 
-    spectrum_signal_av = low_freq_noise_spectrum(spectrum1, 1024)
-    plot_low_freq_spec(spectrum_signal_av, delta_t)
+    path_to_picture = path_to_pic(Path(file_path_data, current_data_file))
+    plot_low_freq_spec(spectrum_signal_av, delta_t, path_to_picture)
 
     pass
