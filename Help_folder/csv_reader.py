9a1e4600bc9c3ef9f5e33ac3d5e28907a038c56f
SAOFastAcquisition <anatoly.ripak@gmail.com>
2023-03-31 18:29:58 +0300

    Программа imf_maker.py выдала первую эмпирическую собственную функцию

--- Help_folder/csv_reader.py
+++ Help_folder/csv_reader.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-path = r'J:\Fast_Acquisition\2023\Interference\2023_03_17_01_200_3500MHz.csv'
+path = r'H:\Fast_Acquisition\2023\Interference\2023_03_18_01_200_3500MHz.csv'
 a = pd.read_csv(path)
 
 a.plot(x='TraceA(Hz)', y='TraceA(dBm)')
--- Help_folder/imf_maker.py
+++ Help_folder/imf_maker.py
@@ -1,13 +1,57 @@
 import numpy as np
 import os
 import sys
-import pandas as pd
 import pickle
 from pathlib import Path
 import matplotlib.pyplot as plt
 from scipy.interpolate import CubicSpline
 from scipy.signal import argrelextrema as agl
 from Help_folder.paths_via_class import DataPaths
+
+
+def imf_gen(_data):
+    from scipy.interpolate import make_interp_spline as mis
+    _l = len(_data)
+    cx = np.array([i for i in range(_l)])
+    _idx_minimas = agl(_data, np.less)[0]
+    _idx_maximas = agl(_data, np.greater)[0]
+
+    # _cs_min = CubicSpline(_idx_minimas, _data[_idx_minimas])
+    l, r = [(1, 0.0)], [(1, 0.0)]
+    _cs_min = mis(_idx_minimas, _data[_idx_minimas], k=3, bc_type=(l, r))
+    # _cs_max = CubicSpline(_idx_maximas, _data[_idx_maximas])
+    _cs_max = mis(_idx_maximas, _data[_idx_maximas], k=3, bc_type=(l, r))
+    _cs = (_cs_max(cx) + _cs_min(cx)) / 2
+
+    _fig = plt.figure(figsize=(12, 8))
+    _axes = _fig.add_subplot()
+    _axes.grid(b=True, which='major', color='#666666', linestyle='-')
+    # Show the minor grid lines with very faint and almost transparent grey lines
+    _axes.minorticks_on()
+    _axes.grid(b=True, which='minor', color='#999999', linestyle='-', alpha=0.5)
+
+    _axes.set_title('Antenna Temperature', fontsize=18)
+    _axes.set_xlabel('Frequency, MHz', fontsize=18)
+    _axes.plot(cx, _data)
+    # axes.plot(idx_minimas, data[idx_minimas])
+    # axes.plot(idx_maximas, data[idx_maximas])
+    _axes.plot(cx, _cs)
+    # axes.plot(xs, cs_max(xs))
+    _axes.plot(cx, _data-_cs)
+    plt.show()
+    return _cs
+
+
+def null_cross_sec(_data):
+    from math import copysign
+    only_ones_list = [copysign(1, element) for element in _data]
+
+    summ = 0
+    for i in range(len(only_ones_list) - 1):
+        summ += abs(only_ones_list[i] + only_ones_list[i + 1])
+
+    _sign_change_count = ((len(only_ones_list) - 1) * 2 - summ) / 2
+    return _sign_change_count
 
 
 if __name__ == '__main__':
@@ -20,16 +64,46 @@ if __name__ == '__main__':
 
     path_npy = Path(str(converted_data_file_path) + '_spectrum.npy')
     spectrum = np.load(path_npy, allow_pickle=True)
-    data = np.log10(spectrum[4, 4:199])
+    data = np.log10(spectrum[4, 5:197])
     l = len(data)
-    arg = np.array([i for i in range(l)])
-    idx_minimas = agl(data, np.less)[0]
-    idx_maximas = agl(data, np.greater)[0]
+    arg = np.arange(0, l, 1)
+    cs = imf_gen(data)
+    data1 = data[arg] - cs
+    sign_change_count1 = null_cross_sec(data1)
+    n_min1 = len(agl(data1, np.less)[0])
+    n_max1 = len(agl(data1, np.greater)[0])
 
-    cs_min = CubicSpline(idx_minimas, data[idx_minimas])
-    cs_max = CubicSpline(idx_maximas, data[idx_maximas])
-    xs = np.arange(0, l, 1)
-    data1 = data[xs] - (cs_max(xs) + cs_min(xs)) / 2
+    cs1 = imf_gen(data1)
+    data2 = data1[arg] - cs1
+    sign_change_count2 = null_cross_sec(data2)
+    n_min2 = len(agl(data2, np.less)[0])
+    n_max2 = len(agl(data2, np.greater)[0])
+
+    cs2 = imf_gen(data2)
+    data3 = data2[arg] - cs2
+    sign_change_count3 = null_cross_sec(data3)
+    n_min3 = len(agl(data3, np.less)[0])
+    n_max3 = len(agl(data3, np.greater)[0])
+
+    cs3 = imf_gen(data3)
+    data4 = data3[arg] - cs3
+    sign_change_count4 = null_cross_sec(data4)
+    n_min4 = len(agl(data4, np.less)[0])
+    n_max4 = len(agl(data4, np.greater)[0])
+
+    cs4 = imf_gen(data4)
+    data5 = data4[arg] - cs4
+    sign_change_count5 = null_cross_sec(data5)
+    n_min5 = len(agl(data5, np.less)[0])
+    n_max5 = len(agl(data5, np.greater)[0])
+
+    cs5 = imf_gen(data5)
+    data6 = data5[arg] - cs5
+    sign_change_count6 = null_cross_sec(data6)
+    n_min6 = len(agl(data5, np.less)[0])
+    n_max6 = len(agl(data5, np.greater)[0])
+
+    cs0 = cs + cs1 + cs2 + cs3 + cs4 + cs5
 
     fig = plt.figure(figsize=(12, 8))
     axes = fig.add_subplot()
@@ -45,8 +119,8 @@ if __name__ == '__main__':
     axes.plot(arg, data)
     # axes.plot(idx_minimas, data[idx_minimas])
     # axes.plot(idx_maximas, data[idx_maximas])
-    # axes.plot(xs, cs_min(xs))
+    axes.plot(arg, cs0)
     # axes.plot(xs, cs_max(xs))
-    axes.plot(arg, data1)
+    axes.plot(arg, data - cs0)
     plt.show()
     pass
